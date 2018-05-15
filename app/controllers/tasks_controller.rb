0104f1f3a6bc01d66665538d43c5ec6e647d060d
giovanni rescaglio <giovannirescaglio@gmail.com>
2018-05-15 11:50:37 +0200

    avancement

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #read
+get 'tasks', to: 'Tasks#index'
+get 'tasks/id', to: 'tasks#show'
+  #create
+get
+post
+
+  #update
+
+
+  #delete
+
 end
+
+
